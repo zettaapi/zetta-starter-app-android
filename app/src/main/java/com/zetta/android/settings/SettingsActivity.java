93d1ae60d2ccf0f0946a61bb416aa1dac784fd19
Paul Blundell <paul@amp.build>
2016-05-28 18:09:13 +0100

    update title to left align & add up buttin

--- app/src/main/java/com/zetta/android/settings/SettingsActivity.java
+++ app/src/main/java/com/zetta/android/settings/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.zetta.android.settings;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.zetta.android.R;
 
@@ -11,5 +13,11 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.findViewById(R.id.toolbar_title).setVisibility(View.GONE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.Settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
     }
 }
