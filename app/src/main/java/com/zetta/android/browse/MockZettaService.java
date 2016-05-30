b3144edf9dbde1420e9d4fee4b6834ab4f5a7d4b
Paul Blundell <paul@amp.build>
2016-05-30 22:49:54 +0100

    moves to SRP for creating background

--- app/src/main/java/com/zetta/android/browse/MockZettaService.java
+++ app/src/main/java/com/zetta/android/browse/MockZettaService.java
@@ -34,91 +34,91 @@ class MockZettaService {
         final List<ListItem> items;
         try {
             items = Arrays.asList(
-                new ListItem.ServerListItem(banglorForegroundColor, getBackgroundDrawableFor(defaultColor), "bangalor"),
+                new ListItem.ServerListItem(banglorForegroundColor, ColorUtil.getBackgroundDrawableFor(defaultColor), "bangalor"),
                 new ListItem.DeviceListItem("Door", "closed",
                                             new URL("http://www.zettaapi.org/icons/door-closed.png"),
                                             banglorForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Photocell", "ready",
                                             new URL("http://www.zettaapi.org/icons/photocell-ready.png"),
                                             banglorForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Security System", "disarmed",
                                             new URL("http://www.zettaapi.org/icons/security-disarmed.png"),
                                             banglorForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Window", "closed",
                                             new URL("http://www.zettaapi.org/icons/window-closed.png"),
                                             banglorForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
-                new ListItem.ServerListItem(newOrleansForegroundColor, getBackgroundDrawableFor(defaultColor), "neworleans"),
+                new ListItem.ServerListItem(newOrleansForegroundColor, ColorUtil.getBackgroundDrawableFor(defaultColor), "neworleans"),
                 new ListItem.DeviceListItem("Motion", "no-motion",
                                             new URL("http://www.zettaapi.org/icons/motion-no-motion.png"),
                                             newOrleansForegroundColor,
-                                            getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
+                                            ColorUtil.getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
                 ),
                 new ListItem.DeviceListItem("Thermometer", "ready",
                                             new URL("http://www.zettaapi.org/icons/thermometer-ready.png"),
                                             newOrleansForegroundColor,
-                                            getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
+                                            ColorUtil.getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
                 ),
                 new ListItem.DeviceListItem("Camera", "ready",
                                             new URL("http://www.zettaapi.org/public/demo/detroit.jpg"),
                                             newOrleansForegroundColor,
-                                            getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
+                                            ColorUtil.getBackgroundDrawableFor(Color.parseColor("#aaaaff"))
                 ),
-                new ListItem.ServerListItem(detroitForegroundColor, getBackgroundDrawableFor(defaultColor), "detroit"),
+                new ListItem.ServerListItem(detroitForegroundColor, ColorUtil.getBackgroundDrawableFor(defaultColor), "detroit"),
                 new ListItem.DeviceListItem("Motion1", "no-motion",
                                             new URL("http://www.zettaapi.org/icons/motion-no-motion.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Thermometer1", "ready",
                                             new URL("http://www.zettaapi.org/icons/thermometer-ready.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Camera1", "ready",
                                             new URL("http://www.zettaapi.org/public/demo/detroit.jpg"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Motion2", "no-motion",
                                             new URL("http://www.zettaapi.org/icons/motion-no-motion.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Thermometer2", "ready",
                                             new URL("http://www.zettaapi.org/icons/thermometer-ready.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Camera2", "ready",
                                             new URL("http://www.zettaapi.org/public/demo/detroit.jpg"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
                 new ListItem.DeviceListItem("Motion3", "no-motion",
                                             new URL("http://www.zettaapi.org/icons/motion-no-motion.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(Color.parseColor("#236e4e"))
+                                            ColorUtil.getBackgroundDrawableFor(Color.parseColor("#236e4e"))
                 ),
                 new ListItem.DeviceListItem("Thermometer3", "ready",
                                             new URL("http://www.zettaapi.org/icons/thermometer-ready.png"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(Color.parseColor("#111111"))
+                                            ColorUtil.getBackgroundDrawableFor(Color.parseColor("#111111"))
                 ),
                 new ListItem.DeviceListItem("Camera3", "ready",
                                             new URL("http://www.zettaapi.org/public/demo/detroit.jpg"),
                                             detroitForegroundColor,
-                                            getBackgroundDrawableFor(defaultColor)
+                                            ColorUtil.getBackgroundDrawableFor(defaultColor)
                 ),
-                new ListItem.ServerListItem(stageForegroundColor, getBackgroundDrawableFor(defaultColor), "stage"),
-                new ListItem.EmptyServerListItem("No devices online for this server", getBackgroundDrawableFor(defaultColor))
+                new ListItem.ServerListItem(stageForegroundColor, ColorUtil.getBackgroundDrawableFor(defaultColor), "stage"),
+                new ListItem.EmptyServerListItem("No devices online for this server", ColorUtil.getBackgroundDrawableFor(defaultColor))
             );
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
@@ -143,46 +143,47 @@ class MockZettaService {
         void on(List<ListItem> listItems);
     }
 
-    private static Drawable getBackgroundDrawableFor(int color) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            StateListDrawable stateListDrawable = new StateListDrawable();
-            stateListDrawable.addState(
-                new int[]{android.R.attr.state_pressed},
-                new ColorDrawable(ColorUtil.lighten(color, 0.30D))
-            );
-            stateListDrawable.addState(
-                new int[]{android.R.attr.state_focused},
-                new ColorDrawable(ColorUtil.darken(color, 0.30D))
-            );
-            stateListDrawable.addState(
-                new int[]{},
-                new ColorDrawable(color)
-            );
-            return stateListDrawable;
-        } else {
-            ColorStateList pressedColor = ColorStateList.valueOf(ColorUtil.lighten(color, 50D));
-            ColorDrawable defaultColor = new ColorDrawable(color);
-            Drawable rippleColor = getRippleMask(color);
-            return new RippleDrawable(
-                pressedColor,
-                defaultColor,
-                rippleColor
-            );
-        }
-    }
-
-    private static Drawable getRippleMask(int color) {
-        float[] outerRadii = new float[8];
-        // 3 is radius of final ripple, instead of 3 you can give required final radius
-        Arrays.fill(outerRadii, 3);
-
-        RoundRectShape r = new RoundRectShape(outerRadii, null, null);
-        ShapeDrawable shapeDrawable = new ShapeDrawable(r);
-        shapeDrawable.getPaint().setColor(color);
-        return shapeDrawable;
-    }
-
     private static class ColorUtil {
+
+        public static Drawable getBackgroundDrawableFor(int color) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                StateListDrawable stateListDrawable = new StateListDrawable();
+                stateListDrawable.addState(
+                    new int[]{android.R.attr.state_pressed},
+                    new ColorDrawable(lighten(color, 0.30D))
+                );
+                stateListDrawable.addState(
+                    new int[]{android.R.attr.state_focused},
+                    new ColorDrawable(darken(color, 0.30D))
+                );
+                stateListDrawable.addState(
+                    new int[]{},
+                    new ColorDrawable(color)
+                );
+                return stateListDrawable;
+            } else {
+                ColorStateList pressedColor = ColorStateList.valueOf(lighten(color, 50D));
+                ColorDrawable defaultColor = new ColorDrawable(color);
+                Drawable rippleColor = getRippleMask(color);
+                return new RippleDrawable(
+                    pressedColor,
+                    defaultColor,
+                    rippleColor
+                );
+            }
+        }
+
+        private static Drawable getRippleMask(int color) {
+            float[] outerRadii = new float[8];
+            // 3 is radius of final ripple, instead of 3 you can give required final radius
+            Arrays.fill(outerRadii, 3);
+
+            RoundRectShape r = new RoundRectShape(outerRadii, null, null);
+            ShapeDrawable shapeDrawable = new ShapeDrawable(r);
+            shapeDrawable.getPaint().setColor(color);
+            return shapeDrawable;
+        }
+
         public static int lighten(int color, double fraction) {
             int red = Color.red(color);
             int green = Color.green(color);
