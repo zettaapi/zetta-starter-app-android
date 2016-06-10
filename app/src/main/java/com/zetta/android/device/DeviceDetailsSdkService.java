b55a19814455d4ee23348515090dbcff4fcec21c
Paul Blundell <paul@amp.build>
2016-06-10 22:21:54 +0100

    Themes the device details page
     - a lot of duplication from the device list at the moment

--- app/src/main/java/com/zetta/android/device/DeviceDetailsSdkService.java
+++ app/src/main/java/com/zetta/android/device/DeviceDetailsSdkService.java
@@ -1,5 +1,8 @@
 package com.zetta.android.device;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 
 import com.apigee.zettakit.ZIKDevice;
@@ -7,21 +10,33 @@ import com.apigee.zettakit.ZIKDeviceId;
 import com.apigee.zettakit.ZIKRoot;
 import com.apigee.zettakit.ZIKServer;
 import com.apigee.zettakit.ZIKSession;
+import com.apigee.zettakit.ZIKStyle;
+import com.apigee.zettakit.ZIKStyleColor;
+import com.apigee.zettakit.ZIKTransition;
 import com.apigee.zettakit.callbacks.ZIKDevicesCallback;
 import com.apigee.zettakit.callbacks.ZIKRootCallback;
 import com.apigee.zettakit.callbacks.ZIKServersCallback;
 import com.novoda.notils.logger.simple.Log;
+import com.zetta.android.ImageLoader;
 import com.zetta.android.ListItem;
 import com.zetta.android.ZettaDeviceId;
+import com.zetta.android.device.actions.ActionMultipleInputListItem;
+import com.zetta.android.device.actions.ActionSingleInputListItem;
+import com.zetta.android.device.actions.ActionToggleListItem;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 class DeviceDetailsSdkService {
+
+    private static final int DEFAULT_BACKGROUND_COLOR = Color.parseColor("#f2f2f2");
+    private static final int DEFAULT_FOREGROUND_COLOR = Color.BLACK;
+
+    private int hierarchicalOneUpBackgroundColor;
+    private int hierarchicalOneUpForegroundColor;
 
     public DeviceDetailsService.Device getDetails(String url, ZettaDeviceId deviceId) {
         return callSdkSynchronously(url, deviceId);
@@ -47,18 +62,44 @@ class DeviceDetailsSdkService {
         };
 
         final List<ListItem> listItems = new ArrayList<>();
-        final CountDownLatch latch = new CountDownLatch(1);
-
         final ZIKSession zikSession = ZIKSession.getSharedSession();
-        zikSession.getRootAsync(url, new ZIKRootCallback() {
+        zikSession.getRootSync(url, new ZIKRootCallback() {
             @Override
             public void onSuccess(@NonNull ZIKRoot root) {
-                zikSession.getServersAsync(root, new ZIKServersCallback() {
+                zikSession.getServersSync(root, new ZIKServersCallback() {
                     @Override
                     public void onSuccess(@NonNull List<ZIKServer> servers) {
                         final ZIKDeviceId zikDeviceId = new ZIKDeviceId(deviceId.getUuid().toString());
                         for (final ZIKServer server : servers) {
-                            zikSession.getDevicesAsync(server, new ZIKDevicesCallback() {
+
+                            ZIKStyle serverStyle = server.getStyle();
+
+                            if (serverStyle == null) {
+                                hierarchicalOneUpForegroundColor = DEFAULT_FOREGROUND_COLOR;
+                                hierarchicalOneUpBackgroundColor = DEFAULT_BACKGROUND_COLOR;
+                            } else {
+                                ZIKStyleColor zikForegroundColor = serverStyle.getForegroundColor();
+                                int serverForegroundColor;
+                                if (zikForegroundColor == null) {
+                                    hierarchicalOneUpForegroundColor = DEFAULT_FOREGROUND_COLOR;
+                                } else {
+                                    String jsonForegroundColor = zikForegroundColor.getHex();
+                                    serverForegroundColor = Color.parseColor(jsonForegroundColor);
+                                    hierarchicalOneUpForegroundColor = serverForegroundColor;
+                                }
+
+                                ZIKStyleColor zikBackgroundColor = serverStyle.getBackgroundColor();
+                                int serverBackgroundColor;
+                                if (zikBackgroundColor == null) {
+                                    hierarchicalOneUpBackgroundColor = DEFAULT_BACKGROUND_COLOR;
+                                } else {
+                                    String jsonBackgroundColor = zikBackgroundColor.getHex();
+                                    serverBackgroundColor = Color.parseColor(jsonBackgroundColor);
+                                    hierarchicalOneUpBackgroundColor = serverBackgroundColor;
+                                }
+                            }
+
+                            zikSession.getDevicesSync(server, new ZIKDevicesCallback() {
                                 @Override
                                 public void onSuccess(@NonNull List<ZIKDevice> devices) {
                                     if (devices.isEmpty()) {
@@ -68,9 +109,78 @@ class DeviceDetailsSdkService {
                                         if (device.getDeviceId().equals(zikDeviceId)) {
                                             listItems.add(new ListItem.HeaderListItem("Actions"));
 
+                                            ZIKStyle deviceStyle = device.getStyle();
+
+                                            int deviceForegroundColor;
+                                            int deviceBackgroundColor;
+                                            if (deviceStyle == null) {
+                                                deviceForegroundColor = hierarchicalOneUpForegroundColor;
+                                                deviceBackgroundColor = hierarchicalOneUpBackgroundColor;
+                                            } else {
+                                                ZIKStyleColor zikForegroundColor = deviceStyle.getForegroundColor();
+                                                if (zikForegroundColor == null) {
+                                                    deviceForegroundColor = hierarchicalOneUpForegroundColor;
+                                                } else {
+                                                    String jsonForegroundColor = zikForegroundColor.getHex();
+                                                    deviceForegroundColor = Color.parseColor(jsonForegroundColor);
+                                                }
+
+                                                ZIKStyleColor zikBackgroundColor = deviceStyle.getBackgroundColor();
+                                                if (zikBackgroundColor == null) {
+                                                    deviceBackgroundColor = hierarchicalOneUpBackgroundColor;
+                                                } else {
+                                                    String jsonBackgroundColor = zikBackgroundColor.getHex();
+                                                    deviceBackgroundColor = Color.parseColor(jsonBackgroundColor);
+                                                }
+                                            }
+
+                                            List<ZIKTransition> transitions = device.getTransitions();
+                                            for (ZIKTransition transition : transitions) {
+                                                List<Map<String, Object>> eventFields = transition.getFields();
+                                                if (eventFields.size() == 1) {
+                                                    String action = transition.getName();
+                                                    ColorStateList actionTextColorList = ColorStateList.valueOf(deviceBackgroundColor);
+                                                    Drawable backgroundDrawable = ImageLoader.Drawables.getBackgroundDrawableFor(deviceForegroundColor);
+                                                    listItems.add(new ActionToggleListItem(action, action, actionTextColorList, backgroundDrawable));
+                                                } else if (eventFields.size() == 2) {
+                                                    Map<String, Object> eventField = eventFields.get(0);
+                                                    String label = String.valueOf(eventField.get("name"));
+                                                    String action = transition.getName();
+                                                    ColorStateList actionTextColorList = ColorStateList.valueOf(deviceBackgroundColor);
+                                                    Drawable backgroundDrawable = ImageLoader.Drawables.getBackgroundDrawableFor(deviceForegroundColor);
+                                                    listItems.add(new ActionSingleInputListItem(label, action, actionTextColorList, backgroundDrawable));
+                                                } else {
+                                                    List<String> labels = new ArrayList<>();
+                                                    for (Map<String, Object> eventField : eventFields) {
+                                                        String label = String.valueOf(eventField.get("name"));
+                                                        labels.add(label);
+                                                    }
+                                                    String action = transition.getName();
+                                                    ColorStateList actionTextColorList = ColorStateList.valueOf(deviceBackgroundColor);
+                                                    Drawable backgroundDrawable = ImageLoader.Drawables.getBackgroundDrawableFor(deviceForegroundColor);
+                                                    listItems.add(new ActionMultipleInputListItem(labels, action, actionTextColorList, backgroundDrawable));
+                                                }
+
+                                            }
+                                            if (transitions.isEmpty()) {
+                                                listItems.add(new PropertyListItem("No actions for this device.", "")); // TODO create a new list type for empty items EmptyListItem?
+                                            }
+
                                             listItems.add(new ListItem.HeaderListItem("Streams"));
 
                                             listItems.add(new ListItem.HeaderListItem("Properties"));
+
+                                            Map<String, Object> deviceProperties = device.getProperties();
+                                            for (String propertyName : deviceProperties.keySet()) {
+                                                if (propertyName.equals("style")) {
+                                                    continue;
+                                                }
+                                                String propertyValue = String.valueOf(deviceProperties.get(propertyName));
+                                                listItems.add(new PropertyListItem(propertyName, propertyValue));
+                                            }
+                                            if (deviceProperties.isEmpty()) {
+                                                listItems.add(new PropertyListItem("No properties for this device.", ""));
+                                            }
 
                                             listItems.add(new ListItem.HeaderListItem("Events"));
 
@@ -90,7 +200,6 @@ class DeviceDetailsSdkService {
                                                     return listItems;
                                                 }
                                             };
-                                            latch.countDown();
                                         }
                                     }
                                 }
@@ -98,7 +207,6 @@ class DeviceDetailsSdkService {
                                 @Override
                                 public void onFailure(@NonNull IOException exception) {
                                     Log.e(exception, "Foobar'd in DeviceListMockService");
-                                    latch.countDown();
                                 }
                             });
                         }
@@ -107,7 +215,6 @@ class DeviceDetailsSdkService {
                     @Override
                     public void onFailure(@NonNull IOException exception) {
                         Log.e(exception, "Foobar'd in DeviceListMockService");
-                        latch.countDown();
                     }
 
                 });
@@ -116,16 +223,9 @@ class DeviceDetailsSdkService {
             @Override
             public void onFailure(@NonNull IOException exception) {
                 Log.e(exception, "Foobar'd in DeviceListMockService");
-                latch.countDown();
             }
 
         });
-
-        try {
-            latch.await(30, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            // will just return an error item
-        }
 
         return callbackHack[0];
     }
